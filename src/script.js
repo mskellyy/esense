a96779c366ed0eeb0490fd5139e2ed13be87792d
Kelly <125195808+mskellyy@users.noreply.github.com>
2025-03-30 02:12:26 -0600

    js form

--- src/script.js
+++ src/script.js
@@ -18,50 +18,6 @@ elementsToAnimate.forEach((element) => observer.observe(element));
 
 // form
 
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("contactForm");
-
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const formData = new FormData(form);
-    let selectedServices = [];
-
-    form
-      .querySelectorAll("input[name='services']:checked")
-      .forEach((checkbox) => {
-        selectedServices.push(checkbox.value);
-      });
-
-    const data = {
-      name: formData.get("name"),
-      email: formData.get("email"),
-      brandName: formData.get("brandName"),
-      website: formData.get("website"),
-      referral: formData.get("referral"),
-      services: selectedServices,
-      vision: formData.get("vision"),
-      budget: formData.get("budget"),
-      launchDate: formData.get("launchDate"),
-      whyUs: formData.get("whyUs"),
-    };
-
-    fetch(
-      "https://script.google.com/macros/s/AKfycbyuGVX4-qEDjsTXuXWuPhZ-wAS_j-PeEkprTiMGcIWaaWpf8gLzQVoXCxT4w-azH3sUfQ/exec",
-      {
-        method: "POST",
-        mode: "no-cors", // This prevents the browser from blocking the request
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      }
-    )
-      .then((response) => console.log("Form submitted successfully"))
-      .catch((error) => console.error("Error:", error));
-  });
-});
-
 // Word animation
 document.addEventListener("DOMContentLoaded", function () {
   const phrases = [
